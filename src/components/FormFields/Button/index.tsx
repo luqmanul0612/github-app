2161861f4ac6987f5c834ae8e17d8976a3a007a9
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-09-21 08:36:20 +0700

    delete router dom

--- src/components/FormFields/Button/index.tsx
+++ src/components/FormFields/Button/index.tsx
@@ -10,7 +10,7 @@ const Button = (props: ButtonProps) => {
     <button
       {...rest}
       className={cn(
-        "box-border min-w-fit flex items-center h-[40px] rounded-[25px] bg-blue-500 px-[25px] text-[13px] font-semibold text-white transition-all duration-200 ease-in-out hover:bg-blue-600 hover:shadow-[0_0_0_2px_theme(colors.blue.300)] disabled:bg-slate-300 disabled:hover:shadow-none",
+        "box-border min-w-fit flex items-center h-[40px] rounded-[4px] bg-blue-500 px-[25px] text-[13px] font-semibold text-white transition-all duration-200 ease-in-out hover:bg-blue-600 hover:shadow-[0_0_0_2px_theme(colors.blue.300)] disabled:bg-slate-300 disabled:hover:shadow-none",
         className,
       )}
     ></button>
--- src/components/FormFields/TextField/index.tsx
+++ src/components/FormFields/TextField/index.tsx
@@ -10,7 +10,7 @@ const TextField = (props: TextFieldProps) => {
     <input
       {...rest}
       className={cn(
-        "box-border disabled:bg-slate-200 disabled:border-slate-300 disabled:text-slate-300 disabled:placeholder:text-slate-300 h-[40px] rounded-[25px] border-[1px] border-slate-200 bg-slate-100 px-[15px] text-[15px] text-slate-700 outline-none transition-all duration-200 ease-in-out placeholder:text-slate-400 focus:border-blue-300 focus:bg-slate-50 focus:shadow-[0_0_0_1px_theme(colors.blue.300)]",
+        "box-border disabled:bg-slate-200 disabled:border-slate-300 disabled:text-slate-300 disabled:placeholder:text-slate-300 h-[40px] rounded-[4px] border-[1px] border-slate-200 bg-slate-100 px-[15px] text-[15px] text-slate-700 outline-none transition-all duration-200 ease-in-out placeholder:text-slate-400 focus:border-blue-300 focus:bg-slate-50 focus:shadow-[0_0_0_1px_theme(colors.blue.300)]",
         className,
       )}
     />
